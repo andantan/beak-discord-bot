a2ad68dfca250fdf193dc0f6c666bb6f176db556
Qbean <kyubin2892@gmail.com>
2023-09-04 20:29:54 +0900

    no message

--- manager.py
+++ manager.py
@@ -4,7 +4,7 @@ from typing import Dict, Any
 
 def read_json() -> Dict[str, Any]:
     try:
-        with open("token.json", "r", encoding="UTF-8") as fp:
+        with open("Data/token.json", "r", encoding="UTF-8") as fp:
             json_data = json.load(fp)
 
     except Exception as e:
