bda1cd29d90620c02fbfcb7a46426050d84d3f79
Qbean <kyubin2892@gmail.com>
2023-09-06 03:31:51 +0900

    Refactor main.py & config.py | config_args and config_env are integrated

--- Config/Initialize/config.py
+++ Config/Initialize/config.py
@@ -6,23 +6,11 @@ import argparse
 from types import FrameType
 from typing import (List, Dict, Any)
 
-from Config.Initialize.vars import ArgumentsNamespace
 
-from Error.exceptions import NotAllowedModule
-
-def config_args() -> ArgumentsNamespace:
-    _allowed_module = "__main__"
-
-    _fl: List[FrameType] = list(sys._current_frames().values())
-    _f: FrameType = _fl.__getitem__(0)
-    _g: Dict[str, Any] = _f.f_back.f_globals
-
-    caller: str = _g.get("__name__")
-
-    if not caller.__eq__(_allowed_module):
-        raise NotAllowedModule(called=caller, allowed=_allowed_module)
-
-    parser = argparse.ArgumentParser(description='Beak discord utility bot arguments')
+def _config_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='Beak discord utility bot arguments'
+    )
 
     parser.add_argument(
         "-d", "--debug",
@@ -43,8 +31,8 @@ def config_args() -> ArgumentsNamespace:
     return parser.parse_args()
     
 
-def config_envs(arguments: ArgumentsNamespace) -> None: 
-    _allowed_module = "__main__"
+def config_envs() -> None: 
+    _allowed_module: str = "__main__"
 
     _fl: List[FrameType] = list(sys._current_frames().values())
     _f: FrameType = _fl.__getitem__(0)
@@ -53,12 +41,13 @@ def config_envs(arguments: ArgumentsNamespace) -> None:
     caller: str = _g.get("__name__")
 
     if not caller.__eq__(_allowed_module):
+        from Error.exceptions import NotAllowedModule
+
         raise NotAllowedModule(called=caller, allowed=_allowed_module)
     
-    if not isinstance(arguments, argparse.Namespace):
-        raise TypeError
+    args: argparse.Namespace = _config_args()
 
     dotenv.load_dotenv(verbose=True)
 
-    for _k, _v in arguments.__dict__.items():
+    for _k, _v in args.__dict__.items():
         os.environ.setdefault(_k, str(_v))
