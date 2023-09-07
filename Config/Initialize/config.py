82bf4f1968e1db36389315bf31befce116c59453
Qbean <kyubin2892@gmail.com>
2023-09-08 01:58:07 +0900

    Revert "September 9, 2023"
    This reverts commit 65555bdc5caee803d7de55f81f2870b0ce8e51a7.

--- Config/Initialize/config.py
+++ Config/Initialize/config.py
@@ -4,7 +4,7 @@ import dotenv
 import argparse
 
 from types import FrameType
-from typing import (List, Dict, Set, Any)
+from typing import (List, Dict, Any)
 
 from Error.exceptions import ConfigException
 
@@ -33,7 +33,7 @@ def _config_args() -> argparse.Namespace:
     return parser.parse_args()
     
 
-def config_envs(initialize: bool=False, **kwargs) -> None: 
+def config_envs() -> None: 
     _allowed_module: str = "__main__"
 
     _fl: List[FrameType] = list(sys._current_frames().values())
@@ -42,29 +42,18 @@ def config_envs(initialize: bool=False, **kwargs) -> None:
 
     caller: str = _g.get("__name__")
 
-
     if not caller.__eq__(_allowed_module):
         raise ConfigException.NotAllowedModule(called=caller, allowed=_allowed_module)
-
-    if initialize:
-        arg_namespace: argparse.Namespace = _config_args()
-        args: Dict[str, Any] = arg_namespace.__dict__
-
-        if args.__getitem__("PATCH") & args.__getitem__("DEBUG"):
-            raise ConfigException.ArgumentDuplication
-
-        dotenv.load_dotenv(verbose=True)
-
-        for _k, _v in args.items():
-            os.environ.setdefault(_k, str(_v))
     
-    if kwargs:
-        config_env_keys: Set[str] = set(kwargs.keys())
-        env_keys: Set[str] = set(os.environ.keys())
+    from Tools.converter import str2bool
 
-        if duplicated_env_keys := env_keys.intersection(config_env_keys):
-            print(duplicated_env_keys)
-        
-        else:
-            print("foo")
+    arg_namespace: argparse.Namespace = _config_args()
+    args: Dict[str, Any] = arg_namespace.__dict__
 
+    if args.__getitem__("PATCH") & args.__getitem__("DEBUG"):
+        raise ConfigException.ArgumentDuplication
+
+    dotenv.load_dotenv(verbose=True)
+
+    for _k, _v in args.items():
+        os.environ.setdefault(_k, str(_v))
--- Error/exceptions.py
+++ Error/exceptions.py
@@ -3,39 +3,7 @@ from typing import (Tuple, Union)
 
 class ConfigException:
     class NotAllowedModule(Exception):
-        def __init__(self, called: str, allowed: Union[str, Tuple[str]]):      
-            self._called_module: str = called
-            self._allowed_module: Union[str, Tuple[str]] = allowed
-
-            super().__init__()
-
-
-        def __str__(self) -> str:
-            if isinstance(self._allowed_module, tuple):
-                return f"Can only be called from module {self._allowed_module} (called module: \'{self._called_module}\')"
-            
-            return f"Can only be called from module \'{self._allowed_module}\' (called module: \'{self._called_module}\')"
-           
-            
-        def __repr__(self) -> str:
-            if isinstance(self._allowed_module, tuple):
-                return f"Allowed module: {self._allowed_module} (called module: \'{self._called_module}\')"
-           
-            return f"Allowed module: \'{self._allowed_module}\' (called module: \'{self._called_module}\')"
-        
-
-        @property
-        def called_module(self) -> str: 
-            return tuple(self._called_module)
-        
-        @property
-        def allowed_module(self) -> Tuple[str]:
-            if isinstance(self._allowed_module, tuple):
-                return self._allowed_module
-            
-            return tuple(self._allowed_module)
-            
-            
- 
+        def __init__(self, called: str, allowed: Union[str, Tuple[str]]):            
+            super().__init__(called, allowed)
 
     class ArgumentDuplication(Exception):...
